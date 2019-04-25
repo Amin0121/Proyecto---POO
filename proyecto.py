5fa4b0acce2217390105b3bfbb69f5ea32c1e0c1
Amin0121 <joseaminmoya21@gmail.com>
2019-04-25 10:55:45 -0400

    .

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,10 @@
+#     zoo1 = Zoo()
+#     zoo1.agregarHabitat()
+#     print(zoo1.habitats[0].__dict__)
+
+#     Habitat1 = Habitat(1,2)
+#     Habitat1.agregarAnimal()
+#     print( .animales[0].__dict__)
 class Animal():
     def __init__(self, animal,especie,nombre):
         self.animal = animal
@@ -8,14 +15,19 @@ class Habitat():
     def __init__(self, nombre, extencion):
         self.nombre = nombre
         self.extencion = extencion
-        self.animales = [ ]
+        self.animales = []
 
     def agregarAnimal(self):
         nombreAnimal = input('Ingresa el nombre del animal:')
         nombreEspecie = input('Ingresa la especie del animal:')
-        nombrePila = input('ingresa el nombre de pila:')
+        nombrePila = input('ingresa el nombre de pila:') 
         nuevoAnimal = Animal(nombreAnimal,nombreEspecie,nombrePila)
         self.animales.append(nuevoAnimal)
+
+    def verAnimal(self):
+        for recorer in self.animales:
+            print (recorer.animal,recorer.extencion,recorer.nombre)
+
 
 class Zoo():
     def __init__(self):
@@ -25,19 +37,37 @@ class Zoo():
         nombreHabitat = input("Ingrese el nombre de habitat")
         extencionHabitat = input("Ingrese la extencion del habitat")
         nuevoHabitat = Habitat(nombreHabitat, extencionHabitat)
-        self.habitats.append(nuevoHabitat)
-    
+        self.habitats.append(nuevoHabitat)                                    
+
     def verhabitat(self):
         for i in self.habitats:
-            print(i.nombre, i.extencion, "\n")
-        
-zoo1 = Zoo()
-zoo1.agregarHabitat()
-print(zoo1.habitats[0].__dict__)
+            print(i)
 
+zoo1 = Zoo()
 Habitat1 = Habitat(1,2)
-Habitat1.agregarAnimal()
-print(Habitat1.animales[0].__dict__)
+
+while True:
+    print('\n\t MENU \n'
+    '1. Crear Habitat\n' 
+    '2. Crear Animal\n'
+    '3. Ver el Habitat\n' 
+    '4. Ver el Animal\n'
+    '5. Salir')  
+    eleccion = input('Seleccionar una opcion')
+
+    if (eleccion == '1'):
+        print('\n\t Datos del Habitat \n')
+        zoo1.agregarHabitat()
+
+    elif(eleccion == '2'): 
+        print('\n\t Datos del Animal \n')
+        Habitat1.agregarAnimal() 
+    
+    elif(eleccion == '5'):
+        print('Gracias por su visita')
+        break
+
+   
 
 
 
--- zoo.py
+++ zoo.py
@@ -1,91 +1,97 @@
 # -*- coding: utf-8 -*-
-# from habitat import *
-# from animls import *
-# miHabitat = None
-# miAnimal = None
-#habitat
+from habitat import *
+from animls import *
+miHabitat = None
+miAnimal = None
+
+listan= []
+
+while True:
+    print('\n\t MENU \n')
+    print('1. Crear Habitat')
+    print('2. Modificar Habitat')
+    print('3. Crear Animal')
+    print('4. Modificar Animal')
+    print('5. Ver el Habitat') 
+    print('6. Ver el Animal')
+    print('7. Salir')
+    eleccion = input('Seleccionar una opcion: ')
+
+    if(eleccion == '1'):
+        print('Datos del habitat')
+        nombreHabitat = input('Nombre del Habitat: ')
+        extencion = input('extencion del Habitat')
+        miHabitat =  Habitat(nombreHabitat,extencion)
+        listan.append(miHabitat)
+
+    elif(eleccion == '2'):
+        print('\n\t Menu para modificar el habitat \n')
+        print('1. Modificar Nombre del Habitat: ')
+        print('2. Modificar extencion del Habitat: ')
+        print('3. Regresar al menu')
+        eleccion2 = input('Seleccionar una opcion: ')
+        if(eleccion2 == '1'):
+            nombreHabitat = input('Nombre del Habitat: ')
+            miHabitat.setnombreHabitat(nombreHabitat)
+        elif(eleccion2 == '2'):
+            extencion = input('extencion del Habitat')
+            miHabitat.setextencion(extencion)
+        elif(eleccion2 == '3'):
+            break
+        else:
+            print('OPCION INCORRECTA PRUEVA OTRA VEZ')
+
+    elif(eleccion == '3'):
+        print('Datos del Animal')
+        animal = input('El Animal es :')
+        especie = input("Especie del Animal:")
+        miAnimal = Animal(animal,especie)
+
+    elif(eleccion == '4'):
+        print('\n\t Menu para modificar los Datos del animal \n')
+        print('1. Modificar Nombre del Animal: ')
+        print('2. Modificar especie del animal: ')
+        print('3. Modificar comida:')
+        print('4. Modificar habilidad:')
+        print('5. Regresar al menu')
+        eleccion3 = input('Seleccionar una opcion: ')
+        if(eleccion3 == '1'):
+            animal= input('Nombre del Animal:')
+            miAnimal.setanimal(animal)
+        elif(eleccion3 == '2'):
+            especie = input("Especie del Animal:")
+            miAnimal.setespecie(especie)
+        elif(eleccion3 == '3'):
+            comida = input("comida del Animal:")
+            miAnimal.setcomida(comida)
+        elif(eleccion3 == '4'):
+            habilidad = input('habilidad del Animal:')
+            miAnimal.sethabilidad(habilidad)
+        elif(eleccion2 == '5'):
+            break
+        else:
+            print('OPCION INCORRECTA PRUEVA OTRA VEZ')
+
+    elif(eleccion == '5'):
+        print('\n\t Habitat\n')
+        print('Habitat:' + miHabitat.getnombreHabitat())
+        print('extencion:' + miHabitat.getextencion())
+        for i in listan:
+            print(i)
+            
 
 
-# while True:
-#     print('\n\t MENU \n')
-#     print('1. Crear Habitat')
-#     print('2. Modificar Habitat')
-#     print('3. Crear Animal')
-#     print('4. Modificar Animal')
-#     print('5. Ver el Habitat') 
-#     print('6. Ver el Animal')
-#     print('7. Salir')
-#     eleccion = input('Seleccionar una opcion: ')
+    elif(eleccion == '6'):
+        print('\n\t Animal\n')
+        print('Animal:' + miAnimal.getanimal())
+        print('Especie:' + miAnimal.getespecie())
+        print('comida:' + miAnimal.getcomida())
+        print('habilidad:' + miAnimal.gethabilidad())
 
-#     if(eleccion == '1'):
-#         print('Datos del habitat')
-#         nombreHabitat = input('Nombre del Habitat: ')
-#         extencion = input('extencion del Habitat')
-#         miHabitat =  Habitat(nombreHabitat,extencion)
+    elif(eleccion == '7'):
+        print('Gracias por su visita')
+        break
 
-#     elif(eleccion == '2'):
-#         print('\n\t Menu para modificar el habitat \n')
-#         print('1. Modificar Nombre del Habitat: ')
-#         print('2. Modificar extencion del Habitat: ')
-#         print('3. Regresar al menu')
-#         eleccion2 = input('Seleccionar una opcion: ')
-#         if(eleccion2 == '1'):
-#             nombreHabitat = input('Nombre del Habitat: ')
-#             miHabitat.setnombreHabitat(nombreHabitat)
-#         elif(eleccion2 == '2'):
-#             extencion = input('extencion del Habitat')
-#             miHabitat.setextencion(extencion)
-#         elif(eleccion2 == '3'):
-#             break
-#         else:
-#             print('OPCION INCORRECTA PRUEVA OTRA VEZ')
-
-#     elif(eleccion == '3'):
-#         print('Datos del Animal')
-#         animal = input('El Animal es :')
-#         especie = input("Especie del Animal:")
-#         miAnimal = Animal(animal,especie)
-
-#     elif(eleccion == '4'):
-#         print('\n\t Menu para modificar los Datos del animal \n')
-#         print('1. Modificar Nombre del Animal: ')
-#         print('2. Modificar especie del animal: ')
-#         print('3. Modificar comida:')
-#         print('4. Modificar habilidad:')
-#         print('5. Regresar al menu')
-#         eleccion3 = input('Seleccionar una opcion: ')
-#         if(eleccion3 == '1'):
-#             animal= input('Nombre del Animal:')
-#             miAnimal.setanimal(animal)
-#         elif(eleccion3 == '2'):
-#             especie = input("Especie del Animal:")
-#             miAnimal.setespecie(especie)
-#         elif(eleccion3 == '3'):
-#             comida = input("comida del Animal:")
-#             miAnimal.setcomida(comida)
-#         elif(eleccion3 == '4'):
-#             habilidad = input('habilidad del Animal:')
-#             miAnimal.sethabilidad(habilidad)
-#         elif(eleccion2 == '5'):
-#             break
-#         else:
-#             print('OPCION INCORRECTA PRUEVA OTRA VEZ')
-
-#     elif(eleccion == '5'):
-#         print('\n\t Habitat\n')
-#         print('Habitat:' + miHabitat.getnombreHabitat())
-#         print('extencion:' + miHabitat.getextencion())
-
-#     elif(eleccion == '6'):
-#         print('\n\t Animal\n')
-#         print('Animal:' + miAnimal.getanimal())
-#         print('Especie:' + miAnimal.getespecie())
-#         print('comida:' + miAnimal.getcomida())
-#         print('habilidad:' + miAnimal.gethabilidad())
-
-#     elif(eleccion == '7'):
-#         print('Gracias por su visita')
-#         break
-
-#     else:
-#         print('OPCION INCORRECTA PRUEVA OTRA VEZ')
+    else:
+        print('OPCION INCORRECTA PRUEVA OTRA VEZ')
+'extencion:' +
